f22db7cf9c69f18ca018a6e35499d2fa27fbbe5b
Claudio Yacarini <cyacarini@rcp.pe>
2019-05-07 16:56:39 -0500

    Add logging

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,13 +1,31 @@
 # project/app/__init__.py
 import falcon
 import json
+import logging
+import os
 from app.tasks import fib
 from celery.result import AsyncResult
 
 
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO")
+logging.basicConfig(level=logging.__getattribute__(LOGLEVEL))
+
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+
+
 class Ping(object):
     def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
+        # resp.status = falcon.HTTP_200
+        _status_code = 200
+        logging.debug("_status_code --> {}".format(_status_code))
+        _status = "HTTP_{}".format(_status_code)
+        logging.debug("_status --> {}".format(_status))
+        resp.status = falcon.__getattribute__(_status)
+        logging.debug("resp.status --> {}".format(resp.status))
         resp.body = json.dumps('pong!')
 
 
